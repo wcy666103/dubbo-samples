604f699c96ff16969e65425d789b06facc149c47
wangcongyang <1601081222@qq.com>
2024-05-28 15:50:51 +0800

    script脚本解读

--- test/scripts/merge-test-results.sh
+++ test/scripts/merge-test-results.sh
@@ -8,14 +8,16 @@ cd $test_dir
 TEST_SUCCESS="TEST SUCCESS"
 TEST_FAILURE="TEST FAILURE"
 TEST_IGNORED="TEST IGNORED"
-
+# 获取这个变量，默认为 8
 JAVA_VER=${JAVA_VER:-8}
 echo "JAVA_VER: $JAVA_VER"
 
+# 这边为什么 在 jobs文件夹下有这些后缀的东西？ 因为ci那边会给测试结果上传，并且在merge job中将结果进行下载，这样
 RESULT_SUFFIX=result-java${JAVA_VER}.txt
 mergedTestResultFile=jobs/merged-test-$RESULT_SUFFIX
 rm -rf $mergedTestResultFile
 
+# 所有测试结果都 打印一下
 echo "All test results:"
 for resultFile in jobs/*$RESULT_SUFFIX; do
   echo "$resultFile:"
@@ -23,6 +25,7 @@ for resultFile in jobs/*$RESULT_SUFFIX; do
   echo ""
 done
 
+# 统计各个类型任务的数量 并且进行输出
 cat jobs/*$RESULT_SUFFIX > $mergedTestResultFile
 successTest=`grep -c "$TEST_SUCCESS" $mergedTestResultFile`
 failedTest=`grep -c "$TEST_FAILURE" $mergedTestResultFile`
--- test/scripts/prepare-test.sh
+++ test/scripts/prepare-test.sh
@@ -2,11 +2,14 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# test 目录在该目录的上一级
 test_dir=$DIR/..
 
+# 如果环境变量JOB_COUNT没有设置，则默认为5
 JOB_COUNT=${JOB_COUNT:-5}
 echo "JOB_COUNT: $JOB_COUNT"
 
+# 清空目录下的东西
 jobs_dir=$test_dir/jobs
 mkdir -p $jobs_dir
 rm -f $jobs_dir/*
@@ -16,19 +19,24 @@ CONFIG_FILE="case-configuration.yml"
 test_list_file=$jobs_dir/testcases.txt
 test_base_dir="$( cd $test_dir/.. && pwd )"
 echo "Searching all '$CONFIG_FILE' under dir $test_base_dir .."
+# grep -v 是排除这个目录下的东西
 find $test_base_dir -name $CONFIG_FILE | grep -v "$test_dir" > $test_list_file
 
 # Split test list into JOB_COUNT parts
 case_index=0
+# while read file 循环是用来从文件或命令输出中逐行读取内容的，在这里就是读取上边的 find输出的内容
 while read file
 do
   job=$((case_index % JOB_COUNT + 1))
   case_index=$((case_index + 1))
+#  使用 ${file%/$CONFIG_FILE}（这是一个 Bash 参数扩展）来从 file 变量中去除文件路径的末尾部分（即 $CONFIG_FILE 的值），这样只保留目录路径。
+#  然后，这个目录路径被追加到相应的作业文件中（$jobs_dir/testjob_${job}.txt）。
   echo ${file%/$CONFIG_FILE} >> $jobs_dir/testjob_${job}.txt
 done < $test_list_file
 
 #Total 94 cases split into 5 jobs:
 echo "Total $case_index cases split into $JOB_COUNT jobs:"
+# 计算每个作业文件中的行数
 grep -r "" -c $jobs_dir
 
 
